a920bc8ebbd552a4d1141ffe6a16d57a7a6a03fb
Dhrunit <jimmyprajapati99@gmail.com>
2021-02-13 17:08:30 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Home from './pages/Home/Home'
+import Skills from './pages/Skills/Skills'
+import About from './pages/About/About'
+import Work from './pages/Work/Work'
 import { Route, Switch } from 'react-router-dom'
 function App() {
 	return (
 		<Switch>
 			<Route path='/' exact component={Home} />
+			<Route path='/about' exact component={About} />
+			<Route path='/skills' exact component={Skills} />
+			<Route path='/work' exact component={Work} />
 		</Switch>
 	)
 }
