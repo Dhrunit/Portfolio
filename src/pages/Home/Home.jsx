1582e11405a663de57d77126a76c8f1c4537fec1
Dhrunit <jimmyprajapati99@gmail.com>
2021-02-13 17:05:28 +0530

    First commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.styles.css'
+
+export default function Home() {
+	return (
+		<div>
+			<nav className='navbar'>
+				<ul>
+					<li>Dhrunit Prajapati</li>
+					<li>Skills</li>
+					<li>About</li>
+					<li>Work</li>
+				</ul>
+			</nav>
+		</div>
+	)
+}
